20e878973ca29b370acbf95766d4f94153731105
germainsafari <GG@ee10725997111>
2023-09-21 18:38:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './styles';
+
+
+const App = () => (
+
+  <div className='bg-primary w-full overflow-hidden'>
+      
+      <div className='{`${styles.paddingX} ${styles.flexCenter}`}'>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+  </div>
+  );
+
+
+export default App
