d9f057946a2f8d6472cf430655f82b58580b610f
germainsafari <GG@ee10725997111>
2023-09-21 19:34:15 +0200

    t

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function FeedbackCard() {
   return (
-    <div>FeedbackCard</div>
+    <div className='flex justify-between flex-col px-10 py-12 rounded - [20px] 
+    max-w-[370px] md:mr-10 sm:mr-0 my-5 feedback-card'>
+
+    </div>
   )
 }
 
