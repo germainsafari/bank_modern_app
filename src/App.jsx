7db38d6f166e7a7b95f57fed25e3ff0a236a4379
germainsafari <GG@ee10725997111>
2023-09-21 18:49:27 +0200

    touch

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,27 @@ const App = () => (
 
   <div className='bg-primary w-full overflow-hidden'>
       
-      <div className='{`${styles.paddingX} ${styles.flexCenter}`}'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           Navbar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+        <div className={`${bg-primary} ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+              Stats
+              Business
+              Billing
+              CardDeal
+              Testimonials
+              Clients
+              CTA
+              Footer
+
+          </div>
         </div>
       </div>
   </div>
