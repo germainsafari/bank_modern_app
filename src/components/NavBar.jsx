9104c8fa85723cd852a89ffcbb4656bae09227ee
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-22 18:05:14 +0200

    touch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,26 @@ import {close, logo, menu} from '../assets';
 import {navLinks} from '../constants'
 
 function Navbar() {
+
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className='w-full flex py-6' justify-between items-center navbar>
       <img src={logo} alt="hookbank" className='w-[124] h-[32px'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li key={nav.id}
-          className={`font-poppins font-normal cursor-pointer text-[16px]`}>
+          className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length ? 'mr-0' : 'mr-10'} text-white mr-10`}>
             <a href={`${nav.id}`}>
               {nav.title}
             </a>
           </li>
         ))}
       </ul>
+      <div className='sm:hidden flex flex-1 justify-end items-center '>
+          <img src={toggle ? close : menu} alt="menu"
+          className='w-[28] h-[28px] object-contain' />
+      </div>
     </nav>
   )
 }
