a524db4303c9f30df721648592f03cf55519f351
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-22 17:59:39 +0200

    touch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,18 @@ import {navLinks} from '../constants'
 
 function Navbar() {
   return (
-    <nav className='w-full flex py-6'>
-
+    <nav className='w-full flex py-6' justify-between items-center navbar>
+      <img src={logo} alt="hookbank" className='w-[124] h-[32px'/>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id}
+          className={`font-poppins font-normal cursor-pointer text-[16px]`}>
+            <a href={`${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
