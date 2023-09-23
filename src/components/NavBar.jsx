7cffc446f11acd3f64bc423f5e0e80f13b748593
germainsafari <GG@ee10725997111>
2023-09-23 18:34:55 +0200

    touch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,16 @@ function Navbar() {
           onClick={() => setToggle((prev) => (!prev))}
           />
           <div className={`${toggle ? 'flex': 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] rounded-xl sidebar` }>
-
+            <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+                {navLinks.map((nav, index) => (
+                  <li key={nav.id}
+                    className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length ? 'mr-0' : 'mr-10'} text-white mr-10`}>
+                    <a href={`${nav.id}`}>
+                      {nav.title}
+                    </a>
+                  </li>
+                ))}
+            </ul>
           </div>
       </div>
     </nav>
